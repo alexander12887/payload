f332833d9c5a0e136c84d031982afdc46cfb18a3
sorrynotsorry <54444533+sorrynotsorry1@users.noreply.github.com>
2021-10-26 16:27:40 +1100

    Update open.sh

--- open.sh
+++ open.sh
@@ -7,7 +7,7 @@ IFOPEN=$(ps aux | grep Terminal)
 if [[ $IFOPEN != *"Terminal.app/Contents/MacOS/Terminal"* ]];
 then
 open -a Terminal &
-osascript -e 'tell application /"Terminal/" to set miniaturized of every window to true'
+osascript -e 'tell application "Terminal" to set miniaturized of every window to true'
 fi
 
 done
